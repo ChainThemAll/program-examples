e2d28bd013c06ed944c7c1dc8273438af397c4b1
alain <zhengze01@gmail>
2024-04-03 18:24:36 +0800

    addcreat_user2

--- basics/close-account/native/program/src/instructions/create_user.rs
+++ basics/close-account/native/program/src/instructions/create_user.rs
@@ -2,7 +2,7 @@ use borsh::BorshSerialize;
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    program::invoke_signed,
+    program::{invoke, invoke_signed},
     pubkey::Pubkey,
     rent::Rent,
     system_instruction,
@@ -39,6 +39,34 @@ pub fn create_user(program_id: &Pubkey, accounts: &[AccountInfo], data: User) ->
             system_program.clone(),
         ],
         &[&[User::SEED_PREFIX.as_bytes(), payer.key.as_ref(), &[bump]]],
+    )?;
+
+    data.serialize(&mut &mut target_account.data.borrow_mut()[..])?;
+    Ok(())
+}
+//which different with create_user?
+pub fn create_user2(program_id: &Pubkey, accounts: &[AccountInfo], data: User) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let target_account = next_account_info(accounts_iter)?;
+    let payer = next_account_info(accounts_iter)?;
+    let system_program = next_account_info(accounts_iter)?;
+
+    let account_span = (data.try_to_vec()?).len();
+    let lamports_required = (Rent::get()?).minimum_balance(account_span);
+
+    invoke(
+        &system_instruction::create_account(
+            payer.key,
+            target_account.key,
+            lamports_required,
+            account_span as u64,
+            program_id,
+        ),
+        &[
+            payer.clone(),
+            target_account.clone(),
+            system_program.clone(),
+        ],
     )?;
 
     data.serialize(&mut &mut target_account.data.borrow_mut()[..])?;
